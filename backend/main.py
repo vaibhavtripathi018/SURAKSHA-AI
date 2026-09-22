c21ffdade7a1d5e579953cd1d7463a3a602910a2
vaibhavtripathi018 <manivaibhav018@gmail.com>
2026-09-22 23:23:31 +0530

    oooo

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:5173",
-    "http://localhost:5174"
+    "http://localhost:5174",
+    "https://suraksha-ai-weld.vercel.app/"
 ],
     allow_credentials=True,
     allow_methods=["*"],
