f48f44ec96205cdb8cd973c6d4e6b71a86c4c21f
vaibhavtripathi018 <manivaibhav018@gmail.com>
2026-09-22 23:38:59 +0530

    littil

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_origins=[
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://suraksha-ai-weld.vercel.app/"
+    "https://suraksha-ai-weld.vercel.app"
 ],
     allow_credentials=True,
     allow_methods=["*"],
